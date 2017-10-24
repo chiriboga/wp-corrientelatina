5feec96c31dbe713f2bc2daa6a2cba8deda7015d
Richard Chiriboga <richardchiriboga@gmail.com>
2017-10-24 17:21:00 -0400

    added google tracking code
    I can't believe I forgot to do that.

--- header.php
+++ header.php
@@ -22,6 +22,14 @@
 <![endif]-->
 <link rel="profile" href="http://gmpg.org/xfn/11">
 <?php wp_head(); ?>
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-725079-1"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+  gtag('config', 'UA-725079-1');
+</script>
 </head>
 <body <?php body_class(); ?>>
 <!-- top ribbon -->
